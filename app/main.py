e8dd8bc1efd0e83b758897805c9d67c70d6fb251
Lukasz_Zywicki <lukasz.zywicki@lhsystems.com>
2020-07-20 16:39:10 +0200

    MODIFY: implement calculating flight, return sorted flights from database

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from flask import jsonify
 from flask_login import login_required
 from flask_login import current_user
 
+from operator import itemgetter
+
 from app import app
 from app import db
 from app import models
@@ -23,11 +25,28 @@ main = Blueprint('main', __name__)
 def inject_variables():
     flights_from_db = Flight.query.all()
     flights_list = [flight.__dict__ for flight in flights_from_db]
-    return {'all_flights': flights_list}
+    # flights_list is a list of dicts, we want them ordered by id to prevent
+    # moving flights on frontend layer i.e. after updating (calculating) flight
+    sorted_flights = sorted(flights_list, key=itemgetter('id'))
+    print(flights_list[0])
+    print(flights_list[1])
+    return {'all_flights': sorted_flights}
 
 @main.app_template_filter('upper')
 def utility_processor(word: str) -> str:
         return word.upper()
+
+@main.app_template_filter('check_status')
+def check_status_return_badge_class(flight_status: str) -> str:
+    """Method that check current flight status and return proper Bootstrap 
+    badge class ('info' for In Progress, 'light' for ' Calculated' flight) 
+    for flight status span element."""
+    if flight_status == 'In Progress':
+        return 'badge-info'
+    elif flight_status == 'Not calculated':
+        return 'badge-warning'
+    elif flight_status == 'Calculated':
+        return 'badge-light'
 
 @main.route('/index')
 @login_required
@@ -87,3 +106,26 @@ def delete_flight():
     db.session.commit()
 
     return redirect(url_for('main.index', user=current_user))
+
+@main.route('/calculate_flight', methods=['POST'])
+@login_required
+def calculate_flight():
+    flight_no = request.form['flight_no'].strip('"')
+    flight_to_calculate = Flight.query.filter_by(flight_no=f'{flight_no}').first()
+
+    flight_to_calculate.status = 'In Progress'
+    db.session.commit()
+
+    fibonacci_recursion(35)
+    print('Flight successfull calculated.')
+
+    flight_to_calculate.status = 'Calculated'
+    db.session.commit()
+
+    return redirect(url_for('main.index', user=current_user))
+
+def fibonacci_recursion(n):
+    if n == 0: return 0
+    if n == 1: return 1
+    
+    return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
