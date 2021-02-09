67f8b9824b21c28a4323922adb26daad9d56a707
U552249 <lukasz.zywicki@lhsystems.com>
2021-02-09 16:02:44 +0100

    ADD: implement waitress for running application

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app import app
+from app import create_app, db
+from app.models import User, Flight, Planet
+from waitress import serve
+
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    serve(app=app, host='0.0.0.0', port='5000', url_scheme='http')
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Flight': Flight, 'Planet': Planet}
